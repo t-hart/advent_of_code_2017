6e6559d1bfba3a5a3788e19165857e1da4c904fd
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2018-08-10 17:15:45 +0200

    Starts day 7

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,7 +1,10 @@
 #![feature(slice_patterns, plugin)]
 #![allow(dead_code)]
 extern crate core;
+#[macro_use]
+extern crate lazy_static;
 extern crate num;
+extern crate regex;
 
 mod day_01;
 mod day_02;
@@ -9,3 +12,4 @@ mod day_03;
 mod day_04;
 mod day_05;
 mod day_06;
+mod day_07;
