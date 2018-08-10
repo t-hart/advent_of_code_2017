3eba25066be2bdc1cc31d1c8f47b46f650ccaa1b
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2018-08-10 20:38:22 +0200

    Solves day 1 in Haskell

--- haskell/src/Day01.hs
+++ haskell/src/Day01.hs
@@ -1,7 +1,22 @@
 module Day01 where
 
-partOne :: (Num a) => [a] -> a
-partOne = sum
+partOne :: (Num a, Eq a) => [a] -> a
+partOne [] = 0
+partOne (x:xs) = helper x (xs ++ [x]) 0
+  where
+    helper _ [] total = total
+    helper y (z:zs) total = helper z zs newTotal
+      where
+        newTotal =
+          if y == z
+            then y + total
+            else total
 
-partTwo :: (Num a) => [a] -> a
-partTwo = partOne
+partTwo :: (Num a, Eq a) => [a] -> a
+partTwo xs = sum $ zipWith add (take half xs) (drop half xs)
+  where
+    half = length xs `div` 2
+    add a b =
+      if a == b
+        then a + b
+        else 0
