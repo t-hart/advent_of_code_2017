9e1bd026aea0c906dad3057e71c92ab32620a25c
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2018-08-11 08:18:05 +0200

    Plays around with day 1 in haskell

--- haskell/src/Day01.hs
+++ haskell/src/Day01.hs
@@ -2,21 +2,17 @@ module Day01 where
 
 partOne :: (Num a, Eq a) => [a] -> a
 partOne [] = 0
-partOne (x:xs) = helper x (xs ++ [x]) 0
-  where
-    helper _ [] total = total
-    helper y (z:zs) total = helper z zs newTotal
-      where
-        newTotal =
-          if y == z
-            then y + total
-            else total
+partOne (x:xs) = sumList const $ zip <*> tail $ (x : xs ++ [x])
 
 partTwo :: (Num a, Eq a) => [a] -> a
-partTwo xs = sum $ zipWith add (take half xs) (drop half xs)
+partTwo xs = sumList (+) $ uncurry zip $ splitAt half xs
   where
     half = length xs `div` 2
-    add a b =
+
+sumList :: (Num a, Eq a) => (a -> a -> a) -> [(a, a)] -> a
+sumList eval = foldl (\acc x -> acc + f x) 0
+  where
+    f (a, b) =
       if a == b
-        then a + b
+        then eval a b
         else 0
