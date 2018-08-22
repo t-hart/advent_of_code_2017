2965d1d2db14defc83b5c32295f9cf64d33dc5e1
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2018-08-22 12:03:20 +0200

    Solves day 4 in Haskell

--- haskell/src/Day04.hs
+++ haskell/src/Day04.hs
@@ -1,13 +1,27 @@
-module Day04 where
+module Day04
+  ( partOne
+  , partTwo
+  , validate
+  , validate'
+  ) where
+
+import qualified Data.HashSet as Set
+import Data.List
+
+solve :: (String -> Bool) -> String -> Int
+solve f = length . filter f . lines
+
+next _ [] = True
+next set (x:xs) = not (Set.member x set) && next (Set.insert x set) xs
 
 partOne :: String -> Int
-partOne = length
+partOne = solve validate
 
 validate :: String -> Bool
-validate _ = True
+validate = next Set.empty . words
 
 partTwo :: String -> Int
-partTwo = length
+partTwo = solve validate'
 
 validate' :: String -> Bool
-validate' _ = False
+validate' = next Set.empty . map sort . words
