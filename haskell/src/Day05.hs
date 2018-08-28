742631e59fad84ca1d04e457f979995466071642
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2018-08-28 12:15:15 +0200

    Solves part one of day 5

--- haskell/src/Day05.hs
+++ haskell/src/Day05.hs
@@ -3,13 +3,19 @@ module Day05
   , partTwo
   ) where
 
-import Data.List
 import qualified Data.Map as Map
 
+escape :: (Integral a, Integral b) => a -> b -> Map.Map a a -> b
+escape index total maze =
+  case Map.lookup index maze of
+    Nothing -> total
+    Just x -> escape (index + x) (total + 1) (Map.insert index (x + 1) maze)
+
+toMaze :: String -> Map.Map Int Int
+toMaze = Map.fromAscList . zip [0 ..] . map read . lines
+
 partOne :: String -> Int
-partOne =
-  length .
-  foldrWithIndex (\i x map -> Map.insert i x map) Map.empty . map read . lines
+partOne = escape 0 0 . toMaze
 
 partTwo :: String -> Int
 partTwo = length
