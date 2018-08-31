db1524ef61c763e17b6c46840f85c6cb527f9415
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2018-08-31 12:08:01 +0200

    Uses insertWith for jumping

--- haskell/src/Day05.hs
+++ haskell/src/Day05.hs
@@ -5,17 +5,29 @@ module Day05
 
 import qualified Data.Map as Map
 
-escape :: (Integral a, Integral b) => a -> b -> Map.Map a a -> b
-escape index total maze =
+escape ::
+     (Integral a, Integral b)
+  => a
+  -> b
+  -> (a -> a -> a -> a)
+  -> Map.Map a a
+  -> b
+escape index total f maze =
   case Map.lookup index maze of
     Nothing -> total
-    Just x -> escape (index + x) (total + 1) (Map.insert index (x + 1) maze)
+    Just x ->
+      escape (index + x) (total + 1) f (Map.insertWith (f x) index 1 maze)
 
 toMaze :: String -> Map.Map Int Int
 toMaze = Map.fromAscList . zip [0 ..] . map read . lines
 
 partOne :: String -> Int
-partOne = escape 0 0 . toMaze
+partOne = escape 0 0 (const (+)) . toMaze
 
 partTwo :: String -> Int
-partTwo = length
+partTwo = escape 0 0 f . toMaze
+  where
+    f x =
+      if x >= 3
+        then (-)
+        else (+)
